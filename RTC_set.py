f0d424a39552f9525ecfb91cac730c809f3766e2
frozzitch <macrackster@gmail.com>
2025-03-04 20:34:41 +0100

    Update RTC_set.py

--- RTC_set.py
+++ RTC_set.py
@@ -7,6 +7,6 @@ i2c=machine.I2C(0,sda=sda, scl=scl, freq=100000)
 
 
 
-rtcset(11,22,33,4,3,25,2,i2c) #Hour, Min, Sec, Day, Month, Year, Day
+rtcset(11,22,33,4,3,25,2,i2c) #Hour, Min, Sec, Day, Month, Year, Day in week (0-6, SUN, MON, TUE...)
 
 print("RTC SET: " + timenow(i2c) + "," + datenow(i2c) + "," + day(i2c))
